2c966241f91b99fa402787a3bbdab94dbc424be1
Ishan Surana <121395061+ishan-surana@users.noreply.github.com>
2023-11-22 00:19:25 +0530

    Update app.py

--- Challenge 1/app.py
+++ Challenge 1/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
